d3078630509079d5f3a1f3be56fc5e1a4ff071cf
ninambash <ninambash@gmail.com>
2023-02-23 19:41:56 -0800

    updated

--- client/script.js
+++ client/script.js
@@ -92,7 +92,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:8000', {
+    const response = await fetch('https://chat-app-nb9r.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
